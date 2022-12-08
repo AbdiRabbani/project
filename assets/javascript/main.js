086941ddf27689e5408a782142145c2f4444e962
MuhammadFaiqAlHarist <alharistfaiq10@gmail.com>
2022-12-08 08:45:51 +0700

    baru

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,34 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    centeredSlides: true,
+    speed: 60000,
+    autoplay: {
+        delay: 25,
+    },
     loop: true,
-    autoplay: true
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 
+});
+
+const swiperr = new Swiper('.swiper', {
+    slidesPerView: 5,
+    centeredSlides: true,
+    slidesPerGroup: 3,
+    spaceBetween: 30,
+    speed: 70000,
+    autoplay: {
+        delay: 20,
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
